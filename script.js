186e23411fdf603e931a5005ac2481b80575d3c9
GAYATHRI S <gayu2952005@gmail.com>
2026-06-04 22:17:19 +0530

    added portfolio upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let homeTabElement = document.getElementById("homeTab");
+let aboutTabElement = document.getElementById("aboutTab");
+let skillTabElement = document.getElementById("skillTab");
+let projectTabElement = document.getElementById("projectTab");
+let contactTabElement = document.getElementById("contactTab");
+
+aboutTabElement.onclick = function(){
+    
+}
